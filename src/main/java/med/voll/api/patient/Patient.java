788acdc4fc2ad5730980b71654b2cda355701182
fabio <fabioviniciuslds@hotmail.com>
2025-01-15 14:23:48 -0300

    Patient Creation, controller, repository, record (DTO) and Entity

---
+++ src/main/java/med/voll/api/patient/Patient.java
@@ -0,0 +1,36 @@
+package med.voll.api.patient;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.address.Address;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Patient")
+@Table(name = "patients")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String cpf;
+    private String telephone;
+
+    @Embedded
+    private Address address;
+
+    public Patient(PatientDataRegistry data) {
+        this.name = data.name();
+        this.email = data.email();
+        this.telephone = data.telephone();
+        this.cpf = data.cpf();
+        this.address = new Address(data.address());
+    }
+}
