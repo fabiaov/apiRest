96c2c3e7d8bda0ff0b704da60cec445ea29dea72
fabio <fabioviniciuslds@hotmail.com>
2025-01-17 11:28:59 -0300

    User login configured, error handler and spring security added

---
+++ src/main/java/med/voll/api/controller/AutenticationController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.user.DataAutentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticationController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity login(@RequestBody @Valid DataAutentication data) {
+        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var autentication = manager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/med/voll/api/domain/user/DataAutentication.java
@@ -0,0 +1,5 @@
+package med.voll.api.domain.user;
+
+public record DataAutentication(String login, String password) {
+
+}
